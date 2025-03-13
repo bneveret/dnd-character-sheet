ce73642223348517ebd092af556bcdf43e3aeb9e
SnowMoto <motogirlmonday@gmail.com>
2025-03-13 14:28:35 -0600

    update swagger to run api-docs

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+router.use('/', (req, res, next) => {
+    res.setHeader ('Content-Type', 'text/html'); // Ensure correct content type next);
+    if (req.path.endsWith('.css')) {
+        res.setHeader('Content-Type', 'text/css');
+      }
+    next();
+    }, swaggerUi.serve, swaggerUi.setup (swaggerDocument));
 
 module.exports = router;
