5308341541f7be4bfe44a9d43c34a7544c2d0cc1
cwallace033 <145495577+cwallace033@users.noreply.github.com>
2025-03-14 08:31:24 -0600

    switching host in docs for video

--- docs/swagger.js
+++ docs/swagger.js
@@ -5,10 +5,10 @@ const doc = {
     title: 'Myth Smith',
     description: 'D&D Character Creator'
   },
-  // host: 'https://mythsmith.onrender.com',
-  // schemes: ['https']
-  host: 'localhost:8080',
-  schemes: ['http']
+  host: 'https://mythsmith.onrender.com',
+  schemes: ['https']
+  // host: 'localhost:8080',
+  // schemes: ['http']
 };
 
 const outputFile = '../swagger.json';
