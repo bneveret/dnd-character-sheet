f81662dba12acb6366822368b0edd94f4fa091f8
cwallace033 <145495577+cwallace033@users.noreply.github.com>
2025-03-17 07:28:53 -0600

    added functions for all controllers

--- controllers/classController.js
+++ controllers/classController.js
@@ -1,24 +1,129 @@
 const mongodb = require('../config/database');
 const { ObjectId } = require("mongodb");
 
+// Get all classes
 const getAll = async (req, res) => {
-    
+  const db = mongodb.getDb();
+  const result = await db.collection('Class').find().toArray();
+  res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
   };
 
+  // Get a single class by ID
   const getSingle = async (req, res) => {
-    
+    try {
+      const classId = new ObjectId(req.params.id);
+      const db = mongodb.getDb();
+      const result = await db.collection('Class').findOne({ _id: classId });
+  
+      if (!result) {
+        return res.status(404).json({ message: 'Class not found' });
+      }
+  
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(result);
+    } catch (error) {
+      console.error('Error getting class by ID', error);
+      return res.status(500).json({ message: 'Failed to retrieve class' });
+    }
   };
 
+  // Create a new class
   const createClass = async (req, res) => {
-    
+    try {
+      const { className, hitDie, primaryAttribute, savingThrows, proficiency, hitpointsAtFirstLevel, spellCastingAttribute, spellsKnown, cantripsKnown, spellSlotsPerLevel, } = req.body;
+
+      if (!className || !hitDie || !primaryAttribute || !savingThrows || !proficiency || !hitpointsAtFirstLevel || !spellCastingAttribute || !spellsKnown || !cantripsKnown || !spellSlotsPerLevel) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+      }
+
+      const newClass = {
+        className,
+        hitDie,
+        primaryAttribute,
+        savingThrows,
+        proficiency,
+        hitpointsAtFirstLevel,
+        spellCastingAttribute,
+        spellsKnown,
+        cantripsKnown,
+        spellSlotsPerLevel,
+
+      };
+
+      const db = mongodb.getDb();
+      const result = await db.collection('Class').insertOne(newClass);
+
+      if (result.acknowledged) {
+        return res.status(201).json({ id: result.insertedId });
+      } else {
+        return res.status(500).json({ message: 'Failed to create class' });
+      }
+
+    } catch (error) {
+      console.error('Error creating class', error);
+      return res.status(500).json({ message: 'Failed to create class' });
+    };
+  
   };
 
+  // Update a class
   const updateClass = async (req, res) => {
-    
+    try {
+      const classId = new ObjectId(req.params.id);
+  
+      const { className, hitDie, primaryAttribute, savingThrows, proficiency, hitpointsAtFirstLevel, spellCastingAttribute, spellsKnown, cantripsKnown, spellSlotsPerLevel } = req.body;
+  
+      if (!className && !hitDie && !primaryAttribute && !savingThrows && !proficiency && !hitpointsAtFirstLevel && !spellCastingAttribute && !spellsKnown && !cantripsKnown && !spellSlotsPerLevel) {
+        return res.status(400).json({ message: 'Please provide at least one field to update' });
+      }
+  
+      const updateInfo = {}
+      if (className) updateInfo.className = className;
+      if (hitDie) updateInfo.hitDie = hitDie;
+      if (primaryAttribute) updateInfo.primaryAttribute = primaryAttribute;
+      if (savingThrows) updateInfo.savingThrows = savingThrows;
+      if (proficiency) updateInfo.proficiency = proficiency;
+      if (hitpointsAtFirstLevel) updateInfo.hitpointsAtFirstLevel = hitpointsAtFirstLevel;
+      if (spellCastingAttribute) updateInfo.spellCastingAttribute = spellCastingAttribute;
+      if (spellsKnown) updateInfo.spellsKnown = spellsKnown;
+      if (cantripsKnown) updateInfo.cantripsKnown = cantripsKnown;
+      if (spellSlotsPerLevel) updateInfo.spellSlotsPerLevel = spellSlotsPerLevel;
+  
+      const db = mongodb.getDb();
+      const result = await db.collection('Class').updateOne(
+        { _id: classId },
+        { $set: updateInfo }
+      );
+  
+      if (result.matchedCount === 1) {
+        return res.status(204).json({ message: 'Class updated' });
+      } else {
+        return res.status(404).json({ message: 'Class not found' });
+      }
+    } catch (error) {
+      console.error('Error updating class', error);
+      return res.status(500).json({ message: 'Failed to update class' });
+    }
   };
 
+  // Delete a class
   const deleteClass = async (req, res) => {
-    
+    try {
+      const classId = new ObjectId(req.params.id);
+
+      const db = mongodb.getDb();
+      const result = await db.collection('Class').deleteOne({ _id: classId });
+  
+      if (result.deletedCount === 1) {
+        return res.status(204).json({ message: 'Class deleted' });
+      } else {
+        return res.status(404).json({ message: 'Class not found' });
+      }
+    } catch (error) {
+      console.error('Error deleting class', error);
+      return res.status(500).json({ message: 'Failed to delete class' });
+    }
   };
 
 module.exports= {getAll, getSingle, createClass, updateClass, deleteClass};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,138 @@
-const mongodb = require('../config/database');
-const { ObjectId } = require("mongodb");
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+const bcrypt = require('bcrypt');
 
-const getAll = async (req, res) => {
+// Get all users
+const getAll = async (req, res, next) => {
+  try {
+    const db = mongodb.getDb();
+    console.log('Connected to database:', db.databaseName); 
 
+    const usersCollection = db.collection('users');
+    console.log('Users Collection:', usersCollection.collectionName); 
+
+    const result = await usersCollection.find().toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error getting users', error);
+    return res.status(500).json({ message: 'Failed to retrieve users' });
+  }
 };
 
-const getSingle = async(req, res) => {
+// Get user by ID
+const getSingle = async (req, res, next) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const db = mongodb.getDb();
+    const usersCollection = db.collection('users');
+    const result = await usersCollection.findOne({ _id: userId });
 
+    if (!result) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error getting user by ID', error);
+    return res.status(500).json({ message: 'Failed to retrieve user' });
+  }
 };
 
-const createUser = async(req, res) => {
+// Create new user
+const createUser = async (req, res, next) => {
+  try {
+    const db = mongodb.getDb();
+    const { firstName, lastName, email, favoriteColor, birthday, timeAvailable, password, phoneNumber } = req.body;
 
+    if (!firstName || !lastName || !email || !favoriteColor || !birthday || !timeAvailable || !password || !phoneNumber) {
+      return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+      firstName,
+      lastName,
+      email,
+      favoriteColor,
+      birthday,
+      timeAvailable,
+      password: hashedPassword,
+      phoneNumber
+    };
+
+    const result = await db.collection('users').insertOne(newUser);
+
+    if (result.acknowledged) {
+      return res.status(201).json({ id: result.insertedId });
+    } else {
+      return res.status(500).json({ message: 'Failed to create user' });
+    }
+
+  } catch (error) {
+    console.error('Error creating user', error);
+    return res.status(500).json({ message: 'Failed to create user' });
+  }
 };
 
-const updateUser = async(req, res) => {
+// Update user by ID
+const updateUser = async (req, res, next) => {
+  try {
+    const db = mongodb.getDb();
 
+    const userId = new ObjectId(req.params.id);
+    const { firstName, lastName, email, favoriteColor, birthday, timeAvailable, password, phoneNumber } = req.body;
+
+    if (!firstName && !lastName && !email && !favoriteColor && !birthday && !timeAvailable && !password && !phoneNumber) {
+      return res.status(400).json({ message: 'Please provide at least one field to update' });
+    }
+
+    const updateInfo = {};
+    if (firstName) updateInfo.firstName = firstName;
+    if (lastName) updateInfo.lastName = lastName;
+    if (email) updateInfo.email = email;
+    if (favoriteColor) updateInfo.favoriteColor = favoriteColor;
+    if (birthday) updateInfo.birthday = birthday;
+    if (timeAvailable) updateInfo.timeAvailable = timeAvailable;
+    if (password) updateInfo.password = password;
+    if (phoneNumber) updateInfo.phoneNumber = phoneNumber;
+
+    const result = await db.collection('users').updateOne(
+      { _id: userId },
+      { $set: updateInfo }
+    );
+
+    if (result.matchedCount === 1) {
+      return res.status(204).json({ message: 'User updated' });
+    } else {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+  } catch (error) {
+    console.error('Error updating user', error);
+    return res.status(500).json({ message: 'Failed to update user' });
+  }
 };
 
-const deleteUser = async(req, res) => {
+// Delete user by ID
+const deleteUser = async (req, res, next) => {
+  try {
+    const db = mongodb.getDb();
 
+    const userId = new ObjectId(req.params.id);
+    const result = await db.collection('users').deleteOne({ _id: userId });
+
+    if (result.deletedCount === 1) {
+      return res.status(204).json({ message: 'User deleted' });
+    } else {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+  } catch (error) {
+    console.error('Error deleting user', error);
+    return res.status(500).json({ message: 'Failed to delete user' });
+  }
 };
 
-module.exports = {getAll, getSingle, createUser, updateUser, deleteUser};
+module.exports = { getAll, getSingle, createUser, updateUser, deleteUser };
