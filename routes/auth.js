c09da38a091d8e4e6cde982919367b2d363626eb
bneveret <eve20003@byui.edu>
2025-04-04 15:12:37 -0600

    oauth fix

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,11 @@ const passport = require('passport');
 const router = express.Router();
 
 // Route to initiate OAuth login
-router.get('/login', passport.authenticate('github2'));
+router.get('/login', passport.authenticate('github'));
 
 // Callback route
 router.get('/callback',
-  passport.authenticate('github2', { failureRedirect: '/' }),
+  passport.authenticate('github', { failureRedirect: '/' }),
   (req, res) => {
     res.redirect('../api-docs');
   }
