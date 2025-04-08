cac9c5cac3c8a4f3cc99880a4feba5590ed65717
bneveret <eve20003@byui.edu>
2025-04-07 21:02:44 -0600

    file directory change

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ router.get('/login', passport.authenticate('github', { scope: ['user:email'] }))
 
 // Callback route
 router.get('/callback',
-  passport.authenticate('github', { failureRedirect: '../../api-docs', successRedirect: '../../api-docs' })
+  passport.authenticate('github', { failureRedirect: '../api-docs', successRedirect: '../api-docs' })
 );
 
 // Route to handle logout
@@ -15,7 +15,7 @@ router.get('/logout', (req, res, next) => {
   req.logout((err) => {
     if (err) { return next(err); }
     req.session.destroy();
-    res.redirect('../../api-docs');
+    res.redirect('../api-docs');
   });
 });
 
